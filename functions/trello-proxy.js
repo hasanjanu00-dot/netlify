8065ec8e50c957885efa2159e8effd827ffa3c33
hasanjanu00-dot <hasanjanu00@gmail.com>
2025-10-11 02:10:14 +0500

    Update trello-proxy.js

--- functions/trello-proxy.js
+++ functions/trello-proxy.js
@@ -1,22 +1,11 @@
-const fetch = require("node-fetch");
+const fetch = require('node-fetch');
 
 const TRELLO_KEY = process.env.TRELLO_KEY;
 const TRELLO_TOKEN = process.env.TRELLO_TOKEN;
 
 exports.handler = async function(event, context) {
-  const headers = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Headers": "Content-Type",
-    "Content-Type": "application/json"
-  };
-
-  // Handle preflight OPTIONS
-  if (event.httpMethod === "OPTIONS") {
-    return { statusCode: 200, body: "OK", headers };
-  }
-
   const { action, boardId, listId, cardId, name, desc } = event.queryStringParameters || {};
-  
+
   let url = "";
   let options = { method: "GET" };
 
@@ -25,29 +14,45 @@ exports.handler = async function(event, context) {
       case "getBoards":
         url = `https://api.trello.com/1/members/me/boards?key=${TRELLO_KEY}&token=${TRELLO_TOKEN}`;
         break;
+
+      case "createBoard":
+        url = `https://api.trello.com/1/boards/?name=${encodeURIComponent(name)}&key=${TRELLO_KEY}&token=${TRELLO_TOKEN}`;
+        options.method = "POST";
+        break;
+
       case "getLists":
         url = `https://api.trello.com/1/boards/${boardId}/lists?cards=all&card_fields=name,desc&key=${TRELLO_KEY}&token=${TRELLO_TOKEN}`;
         break;
+
       case "createCard":
-        url = `https://api.trello.com/1/cards?name=${encodeURIComponent(name)}&desc=${encodeURIComponent(desc || "")}&idList=${listId}&key=${TRELLO_KEY}&token=${TRELLO_TOKEN}`;
+        url = `https://api.trello.com/1/cards?name=${encodeURIComponent(name)}&idList=${listId}&key=${TRELLO_KEY}&token=${TRELLO_TOKEN}`;
         options.method = "POST";
         break;
+
       case "updateCard":
         url = `https://api.trello.com/1/cards/${cardId}?name=${encodeURIComponent(name)}&desc=${encodeURIComponent(desc || "")}&key=${TRELLO_KEY}&token=${TRELLO_TOKEN}`;
         options.method = "PUT";
         break;
+
       case "deleteCard":
         url = `https://api.trello.com/1/cards/${cardId}?key=${TRELLO_KEY}&token=${TRELLO_TOKEN}`;
         options.method = "DELETE";
         break;
+
+      case "moveCard":
+        url = `https://api.trello.com/1/cards/${cardId}?idList=${listId}&key=${TRELLO_KEY}&token=${TRELLO_TOKEN}`;
+        options.method = "PUT";
+        break;
+
       default:
-        return { statusCode: 400, body: JSON.stringify({ error: "Invalid action" }), headers };
+        return { statusCode: 400, body: JSON.stringify({ error: "Invalid action" }) };
     }
 
     const res = await fetch(url, options);
     const data = await res.json();
-    return { statusCode: 200, body: JSON.stringify(data), headers };
-  } catch (err) {
-    return { statusCode: 500, body: JSON.stringify({ error: err.message }), headers };
+    return { statusCode: 200, body: JSON.stringify(data) };
+
+  } catch(err) {
+    return { statusCode: 500, body: JSON.stringify({ error: err.message }) };
   }
 };
